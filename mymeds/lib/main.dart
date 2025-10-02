06fd198bd2daed2b2b255ec3bfa74c7c560cb79a
Pablomm12 <98631018+Pablomm12@users.noreply.github.com>
2025-10-02 18:43:25 -0500

    firebase implementation

--- mymeds/lib/main.dart
+++ mymeds/lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'theme/app_theme.dart';
 import 'routes/app_router.dart';
 
-void main() {
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+  
   runApp(const MyMedsApp());
 }
 
